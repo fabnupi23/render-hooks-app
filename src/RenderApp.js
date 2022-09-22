bbadd16e59064f6d9cd33ba52ad1326aca7d5f24
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-21 23:31:22 -0500

    Ejerci renderCount con useState-useRef-useEffect

--- src/RenderApp.js
+++ src/RenderApp.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 function RenderApp() {
+  const [text, setText] = useState("");
+  // #1 Se crea la referencia, la variable mutable con el valor de 1 
+  const rendersRef= useRef(1); 
+
+  //render 1  ref = 1 
+  useEffect(() => {
+    rendersRef.current++;
+    //render 1 ref++ = 2
+  })
+
+
   return (
     <div>
-        <h1>
-            Soy RenderApp
-        </h1>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+
+        {/* render 1 ref=1 */}
+        <h1>Renders: {rendersRef.current}</h1>
     </div>
   )
 }
